7042c4e645a5b4e90f94318b5e1e99d0df65f89b
Ahmad Yaheaa <ahmad.yahea@gmail.com>
2021-06-06 15:35:04 +0200

    new lines

--- App.js
+++ App.js
@@ -1,21 +1,46 @@
+/* eslint-disable no-unused-vars */
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { View, Text } from 'react-native'
+import firebase from 'firebase'
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import LandingScreen from './components/auth/Landing'
+import RegisterScreen from './components/auth/Register'
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCYlxOyDxugBgqJxHPRDPnqahGhTYxXgGo",
+  authDomain: "instagram-dev-a5d0c.firebaseapp.com",
+  projectId: "instagram-dev-a5d0c",
+  storageBucket: "instagram-dev-a5d0c.appspot.com",
+  messagingSenderId: "568599124970",
+  appId: "1:568599124970:web:f9542ccc41e78387f5f093",
+  measurementId: "G-H80E2MS1JF"
+};
+
+if(firebase.apps.length === 0) {
+    firebase.initializeApp({});
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Stack = createStackNavigator();
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Landing">
+        <Stack.Screen
+          name="Landing"
+          component={LandingScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Register"
+          component={RegisterScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
